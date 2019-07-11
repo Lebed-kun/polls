65ab29b690194bf7bdd2ede76c1386761ea13cc8
Lebed-kun <lebed-kun@mail.ru>
2019-07-11 12:42:27 +0400

    Add create poll form

---
+++ src/forms/CreatePollForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Form, Button, Input, Checkbox } from 'antd';
+
+class CreatePollForm extends React.Component {
+    render() {
+        return (
+            <Form>
+            </Form>
+        )
+    }
+}
+
+export default CreatePollForm;
--- src/pages/AddPollPage.jsx
+++ src/pages/AddPollPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Page from './Page';
+import CreatePollForm from '../forms/CreatePollForm';
 
-// TO DO
 function AddPollPage() {
-    return <Page></Page>
+    // TO DO : add poll form
+    return (
+        <Page title="Create new poll">
+        </Page>
+    )
 }
 
 export default AddPollPage;
