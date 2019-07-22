6d2f9be408b5088bfeaf8415d1d23c8e95fee190
Lebed-kun <lebed-kun@mail.ru>
2019-07-22 14:21:17 +0400

    Change base url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:8000';
+// export const BASE_URL = 'http://localhost:8000'; // For development
+export const BASE_URL = 'https://polls.chiblog.ru'; // For production
 export const MAX_ANSWERS = 10;
 export const MIN_ANSWERS = 2;
 export const CHART_COLORS = 10;
